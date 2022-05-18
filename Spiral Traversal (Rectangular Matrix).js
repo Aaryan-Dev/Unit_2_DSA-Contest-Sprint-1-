cf2bf666809731f082988b60feb00ece81a75fb0
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-05-18 16:42:07 +0530

    Update Spiral Traversal (Rectangular Matrix).js

--- Spiral Traversal (Rectangular Matrix).js
+++ Spiral Traversal (Rectangular Matrix).js
@@ -6,7 +6,39 @@
 
 
 function spiralTraversal(N, M, arr){
-    //write code here
+    function spiralTraversal(N, M, arr){
+    
+let top = 0;
+left =0;
+bottom = N-1;
+right = M-1;
+count = 0;
+bag = "";
+while(count<N*M){
+    for(i=top; i<=bottom && count <N*M ; i++){
+     bag = bag + (arr[i][left])+" ";
+     count++;
+    }
+    left++;
+    for(i=left; i<=right && count <N*M ; i++){
+      bag = bag +(arr[bottom][i])+" ";
+      count++;
+    }
+    bottom--;
+    for(i=bottom; i>=top && count <N*M ; i--){
+         bag = bag +(arr[i][right])+" ";
+    count++;
+    }
+    right--;
+    for(i=right; i>=left && count <N*M; i--){
+         bag = bag +(arr[top][i])+" ";
+        count++;
+    }
+    top++;
+}
+  console.log(bag);
+  
+}
   
 }
 
